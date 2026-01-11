635066ed79f21f09bd8aaba4ae49a2aeeddfcd30
dangross99 <dangross223@gmail.com>
2026-01-11 10:31:19 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -774,7 +774,7 @@ export default function Home() {
                 <img
                   src={encodeURI("/images/ChatGPT Image Jan 10, 2026, 08_04_27 PM.png")}
                   alt="אבן טבעית"
-                  className="w-full h-[414px] md:h-[598px] object-contain bg-transparent translate-y-4 md:translate-y-0 md:scale-[1.4] md:origin-left md:object-left lg:-translate-y-24"
+                  className="w-full h-[414px] md:h-[598px] object-contain bg-transparent translate-y-4 md:translate-y-0 md:scale-[1.4] md:origin-left md:object-left lg:-translate-y-40"
                 />
               </a>
               <div className="absolute inset-x-0 bottom-72 md:bottom-auto md:top-32 lg:top-40 z-10 px-1 text-center" dir="rtl">
@@ -804,7 +804,7 @@ export default function Home() {
                 <img
                   src={encodeURI("/images/ChatGPT Image Jan 10, 2026, 11_01_37 PM.png")}
                   alt="עץ טבעי"
-                  className="w-full h-[414px] md:h-[598px] object-contain bg-transparent translate-y-6 md:translate-y-0 md:scale-[1.4] md:origin-center lg:-translate-y-24"
+                  className="w-full h-[414px] md:h-[598px] object-contain bg-transparent translate-y-6 md:translate-y-0 md:scale-[1.4] md:origin-center lg:-translate-y-40"
                 />
               </a>
               <div className="absolute inset-x-0 bottom-72 md:bottom-auto md:top-32 lg:top-40 z-10 px-1 text-center">

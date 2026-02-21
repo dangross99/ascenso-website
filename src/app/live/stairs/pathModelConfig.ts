c37b8250b33fd2d2369bd48a9377630778f1e078
dan gross <dangross223@gmail.com>
2026-02-21 13:58:04 +0200

    786

--- src/app/live/stairs/pathModelConfig.ts
+++ src/app/live/stairs/pathModelConfig.ts
@@ -104,7 +104,11 @@ export const SEGMENT_CONFIG: Partial<Record<PathKey, Partial<Record<BoxModel, Se
 		ridge: { mirror: false, bodyRotate180: false, forceWallSide: 'left' },
 	},
 	U_0_landing_0: {
-		taper: { mirror: false, bodyRotate180: false },
+		rect: { mirror: false, bodyRotate180: false, landingWalls: [0, 3] },
+		rounded: { mirror: false, bodyRotate180: false, landingWalls: [0, 3] },
+		taper: { mirror: false, bodyRotate180: false, landingWalls: [0, 3] },
+		wedge: { mirror: false, bodyRotate180: false, landingWalls: [0, 3] },
+		ridge: { mirror: false, bodyRotate180: false, landingWalls: [0, 3] },
 	},
 	U_0_landing_1: {
 		taper: { mirror: false, bodyRotate180: false },

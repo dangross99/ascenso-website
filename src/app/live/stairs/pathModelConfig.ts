74d5d744a2b9913c0a392ffd9befa749bf536e91
dan gross <dangross223@gmail.com>
2026-02-22 00:50:58 +0200

    2462

--- src/app/live/stairs/pathModelConfig.ts
+++ src/app/live/stairs/pathModelConfig.ts
@@ -55,7 +55,7 @@ export const SEGMENT_CONFIG: Partial<Record<PathKey, Partial<Record<BoxModel, Se
 	},
 	L_0_flight_1: {
 		ridge: { mirror: true, bodyRotate180: false },
-		taper: { mirror: true, bodyRotate180: false },
+		taper: { mirror: true, bodyRotate180: true },
 		wedge: { mirror: true, bodyRotate180: true },
 	},
 	L_180_flight_0: {

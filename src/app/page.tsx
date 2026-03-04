b1d7973bf43eb751dfb37e5b5ce1a1b05ea477fc
dan gross <dangross223@gmail.com>
2026-03-04 18:19:24 +0200

    28

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -641,10 +641,10 @@ export default function Home() {
           </p>
           <div className="relative w-full rounded-xl overflow-hidden bg-gray-100 aspect-[16/9] min-h-[320px] md:min-h-[400px] lg:min-h-[480px]">
             <ComposableMap
-              projectionConfig={{ scale: 147 }}
+              projectionConfig={{ scale: 320 }}
               style={{ width: "100%", height: "100%" }}
             >
-              <ZoomableGroup center={[20, 25]} zoom={0.5}>
+              <ZoomableGroup center={[20, 20]} zoom={1.15}>
                 <Geographies geography={geoUrl}>
                   {({ geographies }) =>
                     geographies.map((geo) => {

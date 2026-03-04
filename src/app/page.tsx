fde09e8ce96758ebdeb1cf8edf971be1c91eb30d
dan gross <dangross223@gmail.com>
2026-03-04 23:14:30 +0200

    2524

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -887,7 +887,7 @@ export default function Home() {
                 ))}
               </ul>
             </div>
-            <div className="relative z-0 flex-1 min-w-0 rounded-xl overflow-hidden bg-slate-200 aspect-[16/9] min-h-[400px] md:min-h-[500px] lg:min-h-[600px]">
+            <div className="relative z-0 flex-1 min-w-0 rounded-xl overflow-hidden aspect-[16/9] min-h-[400px] md:min-h-[500px] lg:min-h-[600px]">
               <ComposableMap
               projectionConfig={{ scale: 200 }}
               style={{ width: "100%", height: "100%" }}
